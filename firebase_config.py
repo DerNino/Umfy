55b56a624cdd4f0eeacf17901bf5c7788cf0724c
SackUeli <162135768+SackUeli@users.noreply.github.com>
2024-05-16 11:47:04 +0200

    firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Pfad zu Ihrer Firebase-Servicekonto-Schlüsseldatei
-cred = credentials.Certificate("C:\Users\jajod\OneDrive\Desktop\Informatik ZHAW\umfy-53792-firebase-adminsdk-jbrce-399f5d85ba")
+cred = credentials.Certificate("C:\Users\jajod\OneDrive\Desktop\Informatik ZHAW\umfy-53792-firebase-adminsdk-jbrce-7bbf56e5de.json")
 firebase_admin.initialize_app(cred)
 
 # Firestore-Client initialisieren
